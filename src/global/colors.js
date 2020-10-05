dc5c9eb39b1edf5b3c83278d0b709c95448ace07
Snippy Valson <snippy.valson@tismotech.com>
2020-10-05 19:00:25 +0530

    Added gpujs benchmarks.

--- src/global/colors.js
+++
@@ -1,8 +0,0 @@
-export var ColorPalletes = [];
-ColorPalletes.push( { background : '#263862' , foreground : '#96b0fc' });
-ColorPalletes.push( { background : '#030f2d' , foreground : '#00bcf9' });
-ColorPalletes.push( { background : '#131314' , foreground : '#91b5a4' });
-ColorPalletes.push( { background : '#140e36' , foreground : '#b82941' });
-ColorPalletes.push( { background : '#2f4650' , foreground : '#91eddf' });
-ColorPalletes.push( { background : '#000000' , foreground : '#63b762' });
-
--- src/views/benchmarks/subviews/sorting_benchmarks/workers/population_worker.js
+++
@@ -1,8 +0,0 @@
-onmessage = function(e) {
-    var count = e.data;
-    var array = [];
-    for(var i = 0; i < count; i++) {
-        array[i] = Math.floor(Math.random() * 100);
-    }
-    postMessage(array);
-}
