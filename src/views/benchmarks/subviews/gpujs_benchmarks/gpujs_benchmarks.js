c2493aa54a30cd42afb7045e4d2879d2c3dbc2b4
Snippy Valson <snippy.valson@tismotech.com>
2020-10-06 19:08:21 +0530

    Added gpujs benchmark section.

--- src/views/benchmarks/subviews/gpujs_benchmarks/gpujs_benchmarks.js
+++ src/views/benchmarks/subviews/gpujs_benchmarks/gpujs_benchmarks.js
@@ -5,61 +5,118 @@ import "./gpujs_benchmarks.css";
 import { getRenderer } from "./../../../gpujs_showcase/kernels/render_kernel";
 import { getColors } from "../../../gpujs_showcase/utils";
 import { Style } from "../../../../global/style";
-import { Array2D } from "./../../../../libs/uitils"
+import { Array2D } from "./../../../../libs/uitils";
+import { getMooreProcess } from "../../../gpujs_showcase/kernels/moore_kernel"
 export class GpuJsBenchmarks extends React.Component {
-
-    currentFps;
     style;
     gpuAutomataState; 
     gpuTempState;
-    rendererCanvas;
     renderer;
+    gridHeight = 3162; 
+    gridWidth = 3162;
+    process;
+    animationHandle;
+    fps_t1 = performance.now();
+    fps_t2 = performance.now();
+    t1 = performance.now();
+    t2 = performance.now();
+    delay = 0;
 
     constructor(props) {
         super();
         this.currentFps = "XX fps";
         this.style = new Style();
-        this.gpuAutomataState = Array2D(2000, 2000);
-        this.gpuTempState = Array2D(2000, 2000);
+        this.style.applyStyle();
+        this.gpuAutomataState = Array2D(this.gridWidth, this.gridHeight);
+        this.gpuTempState = Array2D(this.gridWidth, this.gridHeight);
+        this.state = { rendererCanvas : undefined, currentFps : 0 };
+        this.canvasHolderRef = React.createRef();
+        this.process = getMooreProcess(this.gridWidth, this.gridHeight, 1, 16, 1);
+        this.UpdateRenderer.bind(this);
+        this.animate.bind(this);
     }
 
     componentDidMount() {
-        this.renderer = this.UpdateRenderer(2000,2000, getColors(this.style.getCurrentPallet().background, this.style.getCurrentPallet().foreground, 14));
+        let colors = getColors(this.style.getCurrentPallet().background, this.style.getCurrentPallet().foreground, 14);
+        this.renderer = this.UpdateRenderer(this.gridWidth, this.gridHeight, colors);
     }
     
     UpdateRenderer(width, height, colors) {
       let renderer = getRenderer(width, height, colors);
       let rendererCanvas = renderer.canvas;
-      this.rendererCanvas = rendererCanvas;
+      rendererCanvas.style.maxWidth = '200px';
+      rendererCanvas.style.maxHeight = '200px';
+      rendererCanvas.style.margin = "10px";
+      this.setState( { rendererCanvas : rendererCanvas }, () => {
+        console.log(this.state.rendererCanvas);
+        this.canvasHolderRef.current.appendChild(this.state.rendererCanvas);
+      });
       return renderer;
     }
 
     resetState(numStates) {
-        for (let i = 0; i < rendererHeight; i++) {
-          for (let j = 0; j < rendererWidth; j++) {
+        for (let i = 0; i < this.gridHeight; i++) {
+          for (let j = 0; j < this.gridHeight; j++) {
             let state = Math.floor(Math.random() * numStates);
-            gpuAutomataState[i][j] = state;
+            this.gpuAutomataState[i][j] = state;
           }
         }
         this.renderer(this.gpuAutomataState);
       }
       
     
+    animate() {
+        this.t2 = performance.now();
+        this.fps_t2 = performance.now();
+        if (this.fps_t2 - this.fps_t1 >= 1000) {
+          this.delay = this.t2 - this.t1;
+          this.fps_t1 = this.fps_t2;
+          this.setState({ currentFps : Math.round(1000 / this.delay) });
+        }
+        this.t1 = this.t2;
+        this.gpuTempState = this.process(this.gpuAutomataState);
+        this.renderAndSwap();
+        this.animationHandle = requestAnimationFrame(this.animate.bind(this));
+      }
+
+    renderAndSwap() {
+        this.renderer(this.gpuTempState);
+        let t = this.gpuTempState;
+        this.gpuAutomataState = this.gpuTempState;
+        this.gpuTempState = t;
+      }
+
    onStartClicked() {
-        console.log("Start clicked.");
-        console.log()
+        this.resetState(16);
+        if (this.animationHandle) {
+            cancelAnimationFrame(animationHandle);
+          }
+          this.t1 = performance.now();
+          this.fps_t1 = performance.now();
+          this.animationHandle = requestAnimationFrame(this.animate.bind(this));
     }
 
 
     render() {
         return (
             <div>
-                <div>
-                    <button className = "pixel-button pixel-text-medium start-button" onClick = {this.onStartClicked.bind(this)}>Start</button>
-                    <label className="pixel-text-medium fps-label">FPS : {this.currentFps}</label>
+                <div className="pixel-div section">
+                    <div>
+                        <button className = "pixel-button pixel-text-medium start-button" onClick = {this.onStartClicked.bind(this)}>Start</button>
+                        <label className="pixel-text-medium fps-label">FPS : {this.state.currentFps}</label>
+                    </div>
+                    <div ref = {this.canvasHolderRef} className="render-panel"></div>
+                    <label className="pixel-text-medium fps-label">Average FPS : {this.state.currentFps}</label>
                 </div>
-            <div ref = {this.canvasHolderRef} className="render-panel">{this.rendererCanvas}</div>
-            </div>
+                <div className="pixel-div section">
+                    <div>
+                        <button className = "pixel-button pixel-text-medium start-button" onClick = {this.onStartClicked.bind(this)}>Start</button>
+                        <label className="pixel-text-medium fps-label">FPS : {this.state.currentFps}</label>
+                    </div>
+                    <div ></div>
+                    <label className="pixel-text-medium fps-label">Average FPS : {this.state.currentFps}</label>
+                </div>
+            </div> 
         );
     }
 }
