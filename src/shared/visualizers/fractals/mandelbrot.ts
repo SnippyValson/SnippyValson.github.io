34ea5af161da9f5679f72a11503a005f3ba34736
Snippy Valson <v.snippy@gmail.com>
2021-04-06 11:08:14 +0530

    Added axis visualizer for mandelbrot.

--- src/shared/visualizers/fractals/mandelbrot.ts
+++ src/shared/visualizers/fractals/mandelbrot.ts
@@ -1,3 +1,4 @@
+import { AllHTMLAttributes } from "react";
 import { Array2D, drawMatrix } from "shared/utilities";
 import { IVisualizer } from "../visualizer";
 
@@ -9,9 +10,9 @@ export class MandelbrotVisualzier implements IVisualizer {
         private drawingContext: CanvasRenderingContext2D, private blockSize: number) {
 
         this.planeMatrix = Array2D(Math.round(rowCount), Math.round(columnCount));
-        for (var i = 0; i < Math.round(rowCount); i++) {
-            for (var j = 0; j < Math.round(columnCount); j++) {
-                this.planeMatrix[i][j] = Math.floor(Math.random() * 2);
+        for (var i = 0; i < Math.round(this.rowCount); i++) {
+            for (var j = 0; j < Math.round(this.columnCount); j++) {
+                this.planeMatrix[i][j] = 0;
             }
         }
     }
@@ -20,9 +21,17 @@ export class MandelbrotVisualzier implements IVisualizer {
         let originX = this.columnCount / 2;
         let originY = this.rowCount / 2
         let minorAxisCount = this.rowCount < this.columnCount ? this.rowCount : this.columnCount;
+        
     }
 
     initialize(): void {
+        for (var i = 0; i < Math.round(this.rowCount); i++) {
+            for (var j = 0; j < Math.round(this.columnCount); j++) {
+                if(i == Math.round(this.rowCount / 2) || j == Math.round(this.columnCount / 2)) {
+                    this.planeMatrix[i][j] = 1;
+                }
+            }
+        }
         drawMatrix(this.drawingContext, this.planeMatrix, this.rowCount, this.columnCount, this.blockSize, this.colors);
     }
 }
